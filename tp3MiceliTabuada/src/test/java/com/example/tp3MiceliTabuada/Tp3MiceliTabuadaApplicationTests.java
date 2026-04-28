f1c1fa2fa9b0e2587723af7a63b6d9eca37a4766
Fran <franmiceli08@gmail.com>
2026-04-27 23:57:07 -0300

    descargamos el proyecto base Spring - closes #26

---
+++ tp3MiceliTabuada/src/test/java/com/example/tp3MiceliTabuada/Tp3MiceliTabuadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp3MiceliTabuada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3MiceliTabuadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
